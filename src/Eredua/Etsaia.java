ba4a3604e6f729ddcc9ece31c65a1a73b5acd944
Personal <Personal@192.168.1.40>
2026-03-09 13:03:59 +0100

    Etsaien mugimendua eta Thread

--- src/Eredua/Etsaia.java
+++ src/Eredua/Etsaia.java
@@ -13,26 +13,4 @@ public class Etsaia extends Entitatea {
 	public int getIndizea() {
 		return indizea;
 	}
-	
-	public void mugituRandom() {
-		Random random= new Random();
-		int aukera= random.nextInt(3);
-		int x= this.getPosizioa().getX();
-		int y= this.getPosizioa().getY();
-		
-		
-		if (aukera==0 && mugituDaiteke("ezkerrera")) {
-			//ezkerrerantz mugitu
-			this.getPosizioa().setX(x-1);
-		}
-		if (aukera==1 && mugituDaiteke("eskuinera")) {
-			//eskumarantz mugitu
-			this.getPosizioa().setX(x+1);
-		}
-		if (aukera==2 && mugituDaiteke("behera")) {
-			//beherantz mugitu
-			this.getPosizioa().setY(y+1);
-		}
-	}
-	
 }
