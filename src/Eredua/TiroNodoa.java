f22a86081e84c651318240a0db12b5b3e1137aca
jaracmp <jcampos011@ikasle.ehu.eus>
2026-03-30 18:25:26 +0200

    Composite Tiro

---
+++ src/Eredua/TiroNodoa.java
@@ -0,0 +1,22 @@
+package Eredua;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class TiroNodoa extends Tiro {
+	private List<Tiro> tiroLista = new ArrayList<>();
+	
+	public TiroNodoa (int x, int y) {
+		super (x,y);
+	}
+	public void gehituTiroa(Tiro t) {
+        tiroLista.add(t);
+    }
+	@Override
+	public void run() {
+		for (Tiro t: tiroLista) {
+			t.start();
+		}
+	}
+
+}
