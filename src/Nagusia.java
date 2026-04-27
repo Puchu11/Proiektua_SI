8f60267844a59b7444ed92a654eacc5076109325
andreavasilescu <andreavasilescu@icloud.com>
2026-04-27 13:25:07 +0200

    musikari deitu "nagusia" klasean

--- src/Nagusia.java
+++ src/Nagusia.java
@@ -1,10 +1,13 @@
 import Bista.HasierakoPantaila;
 import Bista.MatrizeB;
+import Eredua.AudioKudeatzailea;
 import Eredua.JokoKudeatzailea;
 import Eredua.MatrizeE;
 
 public class Nagusia {
 	public static void main(String[] args) {
+		AudioKudeatzailea.getAudioKudeatzailea().musikaErreproduzitu("res/intro_musika.wav");
+		
 		HasierakoPantaila hasierakoPantaila = new HasierakoPantaila();
 		MatrizeE matrizeE = MatrizeE.getEma();
 		MatrizeB matrizeB = new MatrizeB();
